52ae51407f25c12659915028a055b8f9738218b0
Duong <llelu1808@gmail.com>
2025-03-29 19:17:03 +0700

    Updated by Lu

--- Code/QuanLyTrungTamNN/QuanLyTrungTamNN/Controllers/STUDENTsController.cs
+++ Code/QuanLyTrungTamNN/QuanLyTrungTamNN/Controllers/STUDENTsController.cs
@@ -1,132 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Net;
 using System.Linq;
-using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using QuanLyTrungTamNN.Models;
 
 namespace QuanLyTrungTamNN.Controllers
 {
-    public class STUDENTsController : Controller
+    public class StudentsController : Controller
     {
-        private TrungTamNgoaiNguEntities1 db = new TrungTamNgoaiNguEntities1 ();
+        private TrungTamNgoaiNguEntities db = new TrungTamNgoaiNguEntities();
 
-        // GET: STUDENTs
+        // GET: Students
         public ActionResult Index()
         {
-            var sTUDENTs = db.STUDENTs.Include(s => s.PARENT);
-            return View(sTUDENTs.ToList());
+            var students = db.STUDENTs.ToList();
+            return View(students);
         }
 
-        // GET: STUDENTs/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            STUDENT sTUDENT = db.STUDENTs.Find(id);
-            if (sTUDENT == null)
-            {
-                return HttpNotFound();
-            }
-            return View(sTUDENT);
-        }
-
-        // GET: STUDENTs/Create
+        // GET: Students/Create
         public ActionResult Create()
         {
-            ViewBag.ParentID = new SelectList(db.PARENTs, "ParentID", "FullName");
             return View();
         }
 
-        // POST: STUDENTs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Students/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "StudentID,FullName,PhoneNumber,Email,DateOfBirth,Address,ParentID")] STUDENT sTUDENT)
+        public ActionResult Create(STUDENT student)
         {
             if (ModelState.IsValid)
             {
-                db.STUDENTs.Add(sTUDENT);
+                db.STUDENTs.Add(student);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            ViewBag.ParentID = new SelectList(db.PARENTs, "ParentID", "FullName", sTUDENT.ParentID);
-            return View(sTUDENT);
+            return View(student);
         }
 
-        // GET: STUDENTs/Edit/5
+        // GET: Students/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            STUDENT sTUDENT = db.STUDENTs.Find(id);
-            if (sTUDENT == null)
-            {
+            STUDENT student = db.STUDENTs.Find(id);
+            if (student == null)
                 return HttpNotFound();
-            }
-            ViewBag.ParentID = new SelectList(db.PARENTs, "ParentID", "FullName", sTUDENT.ParentID);
-            return View(sTUDENT);
+            return View(student);
         }
 
-        // POST: STUDENTs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Students/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "StudentID,FullName,PhoneNumber,Email,DateOfBirth,Address,ParentID")] STUDENT sTUDENT)
+        public ActionResult Edit(STUDENT student)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(sTUDENT).State = EntityState.Modified;
+                db.Entry(student).State = System.Data.Entity.EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.ParentID = new SelectList(db.PARENTs, "ParentID", "FullName", sTUDENT.ParentID);
-            return View(sTUDENT);
+            return View(student);
         }
 
-        // GET: STUDENTs/Delete/5
+        // GET: Students/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            STUDENT sTUDENT = db.STUDENTs.Find(id);
-            if (sTUDENT == null)
-            {
+            STUDENT student = db.STUDENTs.Find(id);
+            if (student == null)
                 return HttpNotFound();
-            }
-            return View(sTUDENT);
+            return View(student);
         }
 
-        // POST: STUDENTs/Delete/5
+        // POST: Students/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            STUDENT sTUDENT = db.STUDENTs.Find(id);
-            db.STUDENTs.Remove(sTUDENT);
+            STUDENT student = db.STUDENTs.Find(id);
+            db.STUDENTs.Remove(student);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
